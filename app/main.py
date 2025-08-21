4f77f39c9a4c8930c41e43e9d63b05ed600a935e
kushalraga <kushalraga20@gmail.com>
2025-08-21 18:27:04 +0530

    basic app + rendering setup

--- app/main.py
+++ app/main.py
@@ -1,27 +1,14 @@
-import os 
-from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import AIMessage, HumanMessage
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-load_dotenv()
+app = FastAPI() 
 
-api_key = os.getenv("GOOGLE_API_KEY")
-if not api_key:
-    raise ValueError("API Key not found")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-prompt_template = ChatPromptTemplate([
-    "system", "You are helpful Real Estate Broker from Dubai with 15+ years of experience on the Dubai RealEstate Market",
-    MessagesPlaceholder("msgs")
-])
+templates = Jinja2Templates(directory="templates")
 
-prompt_template.invoke({"msgs": [HumanMessage(content="hi!")]})
-
-messages_to_pass = [
-    HumanMessage(content="Cost of a 3bhk around Dubai Marina?"),
-    AIMessage(content="The cost of a 3bhk around Dubai Marina is 5M AED"),
-    HumanMessage(content="What about around Burj Khalifa?")
-]
-
-formatted_prompt = prompt_template.invoke({"msgs": messages_to_pass})
-print(formatted_prompt)
+@app.get("/")
+async def home(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("base.html", context)
